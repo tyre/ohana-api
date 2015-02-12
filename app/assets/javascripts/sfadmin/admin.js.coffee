b2670fdc574271988cc8697bfc6992d7e2086426
Grayson Wright <wright.grayson@gmail.com>
2015-02-11 20:21:38 -0800

    Don't edit organizations inline
    This is a reset for the organizations page,
    so that we can reset with a more traditional multi-page
    Rails form approach to editing organizations.

--- app/assets/javascripts/sfadmin/admin.js.coffee
+++ app/assets/javascripts/sfadmin/admin.js.coffee
@@ -1,8 +1,2 @@
 #= require jquery
-#= require best_in_place
-#= require jquery.purr
-#= require best_in_place.purr
 #= require jquery_ujs
-
-jQuery ->
-  $('.best_in_place').best_in_place()
--- spec/features/sfadmin/update_organization_name_spec.rb
+++ spec/features/sfadmin/update_organization_name_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 feature "Update name", :js do
-  scenario "with empty organization name" do
+  pending "with empty organization name" do
     organization = create(:organization)
     login_super_admin
 
@@ -11,7 +11,7 @@ feature "Update name", :js do
     expect(page).to have_content "can't be blank"
   end
 
-  scenario "with valid organization name" do
+  pending "with valid organization name" do
     organization = create(:organization)
     new_name = "Juvenile Sexual Responsibility Program"
     login_super_admin
@@ -21,14 +21,5 @@ feature "Update name", :js do
 
     expect(page).to have_content(new_name)
     expect(organization.reload.name).to eq new_name
-  end
-
-  def change_attribute(attribute, value)
-    find(".best_in_place[data-bip-attribute='#{attribute}']").click
-
-    within("form.form_in_place") { fill_in attribute, with: value }
-    click_button "Save"
-
-    wait_for_ajax
   end
 end
